78342bf4755647b60bc0b6342e9edbc110d63638
Shantam Vijayputra <vshantam@yahoo.com>
2017-06-20 16:31:56 +0530

    Add files via upload

---
+++ opencvimg0.py
@@ -0,0 +1,58 @@
+#--------------------------------Loading and saving image with basic OpenCV Processings----------------------------------------------
+
+
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+
+
+# Loading images to the OpenCv Processing memory..
+Img= #Image with Location
+
+Filters='''Filters can be selected from the below options 
+           1 :- cv2.IMREAD_GRAYSCALE (for removing ALPHA or the degree of opaqueness.) or '0'
+           2 :- cv2.IMREAD_COLOR (for improvising the color BGR/RGB.)  or '1'
+           3 :- cv2.IMREAD_UNCHANGED (No change in image feeding to the memory.)  or '-1'
+        '''
+
+'''Using OPenCv Library....'''
+
+img=cv2.imread(Img,Filters)
+
+cv2.imshow("Processed Image",img)
+cv2.waitKey(0) #Wait for any key to be pressed.
+cv2.destroyAllWindows() #Closes all the opened windows with the processed image.
+
+
+'''Loading Image using Matplotlib Library.....
+      
+
+      
+         img =plt.imshow(img,cmap='options',interpolation='bicubic')
+
+
+         'list of cmap options are below
+                    Possible values are: Accent, Accent_r, Blues, Blues_r, BrBG, BrBG_r, BuGn, BuGn_r, BuPu, BuPu_r, CMRmap, CMRmap_r, Dark2, Dark2_r, GnBu_r, Greens, Greens_r, Greys, Greys_r, OrRd, OrRd_r, Oranges, Oranges_r, PRGn, PRGn_r, Paired, Paired_r, Pastel1, Pastel1_r, Pastel2_r, PiYG, PiYG_r, PuBu, PuBuGn, PuBuGn_r, PuBu_r, PuOr, PuOr_r, PuRd, PuRd_r, Purples, Purples_r, RdBu, RdBu_r, RdGy, RdGy_r  , RdPu, RdPu_r, RdYlBu, RdYlBu_r, RdYlGn, RdYlGn_r, Reds, Reds_r, Set1, Set1_r, Set2, Set2_r, Set3, Set3_r, Spectral, Spectral_r, Vega1, Vega10_r, Vega20, Vega20_r, Vega20b, Vega20b_r, Vega20c, Vega20c_r, Wistia, Wistia_r, YlGn, YlGnBu, YlGnBu_r, YlGn_r, YlOrBr, YlOrBr _r, YlOrRd, YlOrRd_r, afmhot, afmhot_r, autumn, autumn_r, binary, binary_r, bone, bone_r, brg, brg_r, bwr, bwr_r, cool, cool_r, coolwarm, coolwarm_r, copper, copper_r, cubehelix, cubehelix_r, flag, flag_r, gist_earth, gist_earth_r, gist_gray, gist_gray_r, gist_heat, gist_heat_r, gist_ncar, gist_ncar_r, gist_rainbow, gist_rainbow_r, gist_stern, gist_stern_r, gist_yarg, gist_yarg_r, gnuplot, gnuplot2, gnuplot2_r, gnuplot_r, gray, gray_r, hot, hot_r, hsv, hsv_r, inferno, inferno_r, jet, jet_r, magma, magma_r, nipy_spectral, nipy_spectral_r, ocean, ocean_r, pink, pink_r, plasma, plasma_r, prism, prism_r, rainbow, rainbow_r, seismic, seismic_r, spectral, spectral_r, spring, spring_r, summer, summer_r, terrain, terrain_r, viridis, viridis_r, winter, winter_r'
+    
+
+        plt.show()
+
+'''
+#Saving the processed image..
+
+dest=#Location of image to be saved..
+
+cv2.imwrite(dest,img)
+
+
+#PLotting Data points on the image (but can be done only with the help of matplotli.pyplot modules.
+
+co-ords=(,)
+plt.plot(co-ords,'#color',linewidth=can_be_any_value)
+
+plt.show() #will show the image with plotted points.
+
+
+
